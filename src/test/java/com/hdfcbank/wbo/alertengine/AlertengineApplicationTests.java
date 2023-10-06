ae0ed872c22241a4590cbef3dd77ec2b7e457710
sujitrs <suryawanshisujeet@gmail.com>
2023-10-06 15:51:40 +0530

    Add files via upload

---
+++ src/test/java/com/hdfcbank/wbo/alertengine/AlertengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfcbank.wbo.alertengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
